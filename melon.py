200b76ada337567552e59ac362aab04a5816620d
pcm422 <pcm0422@gmail.com>
2024-12-30 17:39:41 +0900

    ✨ 과제 완성
    멜론 순위를 스크래핑하여 1위부터 100위까지 제목 가수 등을 출력하는 코드

---
+++ melon.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+header_user = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+url = 'https://www.melon.com/chart/index.htm'
+
+rep = requests.get(url, headers=header_user)
+html = rep.text
+soup = BeautifulSoup(html, 'html.parser')
+lst50 = soup.select('.lst50')
+lst100 = soup.select('.lst100')
+lst_all = lst50 + lst100
+
+for i in lst_all:
+    rank = i.select_one('.rank').text
+    title = i.select_one('.ellipsis.rank01 span a').text
+    singer = i.select_one('.ellipsis.rank02 span a').text
+    album = i.select_one('.ellipsis.rank03 a').text
+    print(f"순위 : {rank}")
+    print(f"제목 : {title}")
+    print(f"가수 : {singer}")
+    print(f"앨범 : {album}")
+    print('--------------------------------------------------')
